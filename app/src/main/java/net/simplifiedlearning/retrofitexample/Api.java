421f0938e3052f427b92c8e2c2b972d5dfc4e33f
belal <belal@wizely.in>
2020-09-30 19:42:34 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedlearning/retrofitexample/Api.java
@@ -0,0 +1,27 @@
+package net.simplifiedlearning.retrofitexample;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Belal on 10/2/2017.
+ */
+
+public interface Api {
+
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    /**
+     * The return type is important here
+     * The class structure that you've defined in Call<T>
+     * should exactly match with your json response
+     * If you are not using another api, and using the same as mine
+     * then no need to worry, but if you have your own API, make sure
+     * you change the return type appropriately
+     **/
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+
+}
